5b427d6c3dd25407333fb19ec77a981e9d013e13
barry779 <barry779@gmail.com>
2016-03-04 14:03:09 +0800

    wx token

---
+++ Token.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * wechat php test
+ */
+//define your token
+define("TOKEN", "barry");
+
+$wechatObj = new Token();
+$wechatObj->valid();
+class Token{
+    public function valid() {
+        $echoStr = $_GET["echostr"];
+        if ($this->checkSignature()) {
+            echo $echoStr;
+            exit;
+        }
+    }
+    private function checkSignature() {
+        $signature = $_GET["signature"];
+        $timestamp = $_GET["timestamp"];
+        $nonce = $_GET["nonce"];
+
+        $token = TOKEN;
+        $tmpArr = array($token, $timestamp, $nonce);
+        sort($tmpArr, SORT_STRING);
+        $tmpStr = implode($tmpArr);
+        $tmpStr = sha1($tmpStr);
+        if ($tmpStr == $signature) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+?>
