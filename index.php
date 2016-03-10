e8ccfc14d0c9e67c4596efca3c1d2af048d9ea71
sunbo779 <sunbo779@163.com>
2016-03-10 15:09:04 +0800

    自动回复

--- index.php
+++ index.php
@@ -1,21 +1,26 @@
 <?php
-/**
- * wechat php test
- */
-//define your token
 define("TOKEN", "barry");
+$wechatObj = new wechatCallbackapiTest();
+if (isset($_GET['echostr'])) {
+    $wechatObj->valid();
+}else{
+    $wechatObj->responseMsg();
+}
 
-$wechatObj = new Token();
-$wechatObj->valid();
-class Token{
-    public function valid() {
+class wechatCallbackapiTest
+{
+    public function valid()
+    {
         $echoStr = $_GET["echostr"];
-        if ($this->checkSignature()) {
+        if($this->checkSignature()){
+            header('content-type:text');
             echo $echoStr;
             exit;
         }
     }
-    private function checkSignature() {
+
+    private function checkSignature()
+    {
         $signature = $_GET["signature"];
         $timestamp = $_GET["timestamp"];
         $nonce = $_GET["nonce"];
@@ -23,12 +28,39 @@ class Token{
         $token = TOKEN;
         $tmpArr = array($token, $timestamp, $nonce);
         sort($tmpArr, SORT_STRING);
-        $tmpStr = implode($tmpArr);
-        $tmpStr = sha1($tmpStr);
-        if ($tmpStr == $signature) {
+        $tmpStr = implode( $tmpArr );
+        $tmpStr = sha1( $tmpStr );
+
+        if( $tmpStr == $signature ){
             return true;
-        } else {
+        }else{
             return false;
+        }
+    }
+
+    public function responseMsg()
+    {
+        $postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
+
+        if (!empty($postStr)){
+            $postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
+            $fromUsername = $postObj->FromUserName;
+            $toUsername = $postObj->ToUserName;
+            $keyword = trim($postObj->Content);
+            $time = time();
+                    $textTpl = "<xml><ToUserName><![CDATA[%s]]></ToUserName><FromUserName><![CDATA[%s]]>".
+			"</FromUserName><CreateTime>%s</CreateTime><MsgType><![CDATA[%s]]></MsgType><Content>".
+			"<![CDATA[%s]]></Content><FuncFlag>0</FuncFlag></xml>";
+            if($keyword == "1")
+            {
+                $msgType = "text";
+                $contentStr = date("Y-m-d H:i:s",time());
+                $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+                echo $resultStr;
+            }
+        }else{
+            echo "";
+            exit;
         }
     }
 }
